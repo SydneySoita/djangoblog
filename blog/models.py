56f7ca4ccdcce38ac5d55e141b8e67c5efc23121
SydneySoita <sydneysoita.ss@gmail.com>
2023-04-23 23:37:43 +0300

    Django Blog App

---
+++ blog/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    title  = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments = True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_detail' , kwargs = {'pk', self.pk})
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='Comments',on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+    
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+
+    
